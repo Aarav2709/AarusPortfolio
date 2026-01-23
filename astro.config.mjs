5180036a5fe95532cd1b95485bd0a9d559c98016
Aarav Gupta <tribejustice35@gmail.com>
2026-01-23 15:45:31 +0530

    Minor bug fixes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   site: 'https://aarus2709.me',
@@ -9,6 +10,15 @@ export default defineConfig({
     inlineStylesheets: 'auto',
   },
   vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+        '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      },
+    },
     build: {
       cssMinify: 'lightningcss',
       rollupOptions: {
